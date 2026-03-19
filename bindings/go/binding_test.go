28275732abafa0aced0ab4981d423cb382e02147
Matouš Dzivjak <matousdzivjak@gmail.com>
2026-03-19 19:27:25 +0100

    feat: update everything to latest

--- bindings/go/binding_test.go
+++ bindings/go/binding_test.go
@@ -3,8 +3,8 @@ package tree_sitter_fga_test
 import (
 	"testing"
 
-	tree_sitter "github.com/smacker/go-tree-sitter"
-	"github.com/tree-sitter/tree-sitter-fga"
+	tree_sitter "github.com/tree-sitter/go-tree-sitter"
+	tree_sitter_fga "github.com/matoous/tree-sitter-fga/bindings/go"
 )
 
 func TestCanLoadGrammar(t *testing.T) {
